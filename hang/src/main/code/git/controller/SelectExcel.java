e57d72e70530e268fe7bbc9401b971a14a15f700
张宇航 <1765887945@qq.com>
2017-10-19 17:27:48 +0800

    重构

--- hang/src/main/code/git/controller/SelectExcel.java
+++ hang/src/main/code/git/controller/SelectExcel.java
@@ -1,6 +1,5 @@
 package git.controller;
 
-import java.io.File;
 import java.io.IOException;
 import java.sql.SQLException;
 import org.eclipse.jgit.api.errors.GitAPIException;
@@ -19,41 +18,37 @@ import git.PropertiesFile;
 public class SelectExcel {
 	@Autowired
 	private UpdateResource upResource;
-	
+
 	@RequestMapping("/gitpush")
 	public String list(Model model) throws SQLException, IOException {
 		return "excelselect";
 	}
-	
-	
+
 	@RequestMapping(value = "push", method = RequestMethod.POST)
-	public synchronized String UploadSourceFile(Model model,String version) throws IOException, SQLException, GitAPIException {
+	public synchronized String UploadSourceFile(Model model, String version)
+			throws IOException, SQLException, GitAPIException {
 		System.out.println(version);
-		if(version.equals("")) {
+		
+		if (version.equals("")) {
 			System.out.println("版本为空");
 			return "error";
 		}
+		
 		upResource.svnCheckOut(version);
-		File file = new File(PropertiesFile.getInstance().getProperty("gitAddress"));
-		if(!file.exists()) {
-			System.out.println(file.getAbsolutePath()+"不存在");
-			return "error";
-		}
+		
 		GitCommand gitCommand = GitCommand.getInstance();
-		
-		gitCommand.gitCheckout(file,version);
-	
 
-		gitCommand.gitPull(file);
-		System.out.println("生成文件*******"+ file.getAbsolutePath());
+		gitCommand.gitCheckout(version);
+		gitCommand.gitPull();
+
+		System.out.println("生成文件***************************" );
 		String[] args = { PropertiesFile.getInstance().getProperty("gitAddress") };
-		Transfer.exce(args,version);
-		
-		
+		Transfer.exce(args, version);
+
 		try {
-			gitCommand.gitAdd(file);
-			gitCommand.gitCommit(file);                                                                                                                    
-			gitCommand.gitPush(file);
+			gitCommand.gitAdd();
+			gitCommand.gitCommit();
+			gitCommand.gitPush();
 		} catch (GitAPIException e) {
 			System.out.println(e.getMessage());
 			e.printStackTrace();
--- hang/src/main/code/git/git_util/GitCommand.java
+++ hang/src/main/code/git/git_util/GitCommand.java
@@ -3,23 +3,22 @@ package git.git_util;
 import java.io.File;
 import java.io.IOException;
 import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.PullCommand;
-import org.eclipse.jgit.api.errors.CheckoutConflictException;
 import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.api.errors.InvalidRefNameException;
 import org.eclipse.jgit.api.errors.JGitInternalException;
-import org.eclipse.jgit.api.errors.RefAlreadyExistsException;
-import org.eclipse.jgit.api.errors.RefNotFoundException;
 import org.eclipse.jgit.internal.storage.file.FileRepository;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
+
+import git.PropertiesFile;
 
 public class GitCommand {
 
 	private final String USR = "zhangyuhang";
 	private final String PASSWORD = "yuhang1994.1118";
 	private static GitCommand instance = null;
-	private  UsernamePasswordCredentialsProvider usernamePasswordCredentialsProvider;
+	private final UsernamePasswordCredentialsProvider usernamePasswordCredentialsProvider;
+	private final File repoFile;
+	private Git git;
 
 	public static synchronized GitCommand getInstance() {
 		if (instance == null) {
@@ -27,11 +26,37 @@ public class GitCommand {
 		}
 		return instance;
 	}
-	
+
 	private GitCommand() {
 		usernamePasswordCredentialsProvider = new UsernamePasswordCredentialsProvider(USR, PASSWORD);
+		repoFile = new File(PropertiesFile.getInstance().getProperty("gitAddress") + "/.git");
+		if (!repoFile.exists()) {
+			System.out.println("该文件不存在" + repoFile.getAbsolutePath());
+		}
 	}
-	
+
+	/**
+	 * 得到git
+	 */
+	private Git getGit() {
+		Repository repo = null;
+		try {
+			repo = new FileRepository(repoFile);
+			git = new Git(repo);
+			if (git == null) {
+				System.out.println("git is null");
+				return null;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (repo != null) {
+				repo.close();
+			}
+		}
+		return git;
+	}
+
 	public boolean gitClone(String remoteUrl, File repoDir) {
 		try {
 			Git git = Git.cloneRepository().setCredentialsProvider(usernamePasswordCredentialsProvider)
@@ -44,88 +69,54 @@ public class GitCommand {
 		return true;
 	}
 
-	@SuppressWarnings("resource")
-	public void gitCheckout(File repoDir, String version) {
-		File RepoGitDir = new File(repoDir.getAbsolutePath() + "/.git");
-		if (!RepoGitDir.exists()) {
-			System.out.println("Error! Not Exists : " + RepoGitDir.getAbsolutePath());
-		} else {
-			Repository repo = null;
-			try {
-				repo = new FileRepository(RepoGitDir.getAbsolutePath());
-				Git git = new Git(repo);
-				git.checkout()
-					.setName(version)
-					.call();
-				System.out.println("Checkout to " + version);
-			} catch (IOException e) {
-				e.printStackTrace();
-			} catch (RefAlreadyExistsException e) {
-				e.printStackTrace();
-			} catch (RefNotFoundException e) {
-				e.printStackTrace();
-			} catch (InvalidRefNameException e) {
-				e.printStackTrace();
-			} catch (CheckoutConflictException e) {
-				e.printStackTrace();
-			} catch (GitAPIException e) {
-				e.printStackTrace();
-			}
-			System.out.println("Checkout to " + version);
+	/**
+	 * 切换分支
+	 */
+	public void gitCheckout(String version) {
+		try {
+			getGit().checkout().setName(version).call();
+		} catch (GitAPIException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Checkout to " + version);
+	}
+
+	/**
+	 * 拉取,拉取两次
+	 */
+	public void gitPull() {
+		try {
+			getGit().pull().setCredentialsProvider(usernamePasswordCredentialsProvider).call();
+			getGit().pull().setCredentialsProvider(usernamePasswordCredentialsProvider).call();
+
+			System.out.println("Pulled from remote repository to local repository at " + repoFile.getAbsolutePath());
+		} catch (Exception e) {
+			System.out.println(e.getMessage() + " : " + repoFile.getAbsolutePath());
 		}
 	}
 
-	@SuppressWarnings("resource")
-	public void gitPull(File repoDir) {
-		File RepoGitDir = new File(repoDir.getAbsolutePath() + "/.git");
-		if (!RepoGitDir.exists()) {
-			System.out.println("Error! Not Exists : " + RepoGitDir.getAbsolutePath());
-		} else {
-			Repository repo = null;
-			try {
-				repo = new FileRepository(RepoGitDir.getAbsolutePath());
-				Git git = new Git(repo);
-				PullCommand pullCmd = git.pull();
-				pullCmd.setCredentialsProvider(usernamePasswordCredentialsProvider).call();
-				pullCmd.setCredentialsProvider(usernamePasswordCredentialsProvider).call();
-				
-				System.out.println("Pulled from remote repository to local repository at " + repo.getDirectory());
-			} catch (Exception e) {
-				System.out.println(e.getMessage() + " : " + RepoGitDir.getAbsolutePath());
-			} finally {
-				if (repo != null) {
-					repo.close();
-				}
-			}
-		}
-	}
-
-	@SuppressWarnings("resource")
-	public void gitCommit(File file) throws IOException, GitAPIException, JGitInternalException {
-		Git git = new Git(new FileRepository(file.getAbsolutePath() + "/.git"));
-		git.commit().setMessage("推表").call();
-		System.out.println("commit:" + file.getAbsolutePath() + "/.git");
+	/**
+	 * 提交
+	 */
+	public void gitCommit() throws IOException, GitAPIException, JGitInternalException {
+		getGit().commit().setMessage("推表").call();
+		System.out.println("commit:" + repoFile.getAbsolutePath());
 	}
 
 	/**
 	 * 本地仓库新增文件
 	 */
-	@SuppressWarnings("resource")
-	public void gitAdd(File file) throws IOException, GitAPIException {
-		Git git = new Git(new FileRepository(file.getAbsolutePath() + "/.git"));
-		git.add().addFilepattern(".").call();
-		System.out.println("add:" + file.getAbsolutePath() + "/.git");
+	public void gitAdd() throws IOException, GitAPIException {
+		getGit().add().addFilepattern(".").call();
+		System.out.println("add:" + repoFile.getAbsolutePath());
 	}
 
 	/**
 	 * push本地代码到远程仓库地址
 	 */
-	@SuppressWarnings("resource")
-	public void gitPush(File file) throws IOException, JGitInternalException, GitAPIException {
-
-		Git git = new Git(new FileRepository(file.getAbsolutePath() + "/.git"));
-		git.push().setRemote("origin").setCredentialsProvider(usernamePasswordCredentialsProvider).call();
-		System.out.println("push:" + file.getAbsolutePath() + "/.git");
+	public void gitPush() throws IOException, JGitInternalException, GitAPIException {
+		getGit().push().setRemote("origin").setCredentialsProvider(usernamePasswordCredentialsProvider).call();
+		System.out.println("push:" + repoFile.getAbsolutePath());
 	}
 
 }
